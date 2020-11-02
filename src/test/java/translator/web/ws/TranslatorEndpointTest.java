12bfe3a7602bef154ad862896e625ae1c2a8fd06
Jorge Turbica <723883@unizar.es>
2020-11-02 17:11:07 +0100

    Test for remote service failures created

--- src/test/java/translator/web/ws/TranslatorEndpointTest.java
+++ src/test/java/translator/web/ws/TranslatorEndpointTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.ClassUtils;
+import org.springframework.ws.client.WebServiceTransportException;
 import org.springframework.ws.client.core.WebServiceTemplate;
 
 import translator.Application;
@@ -44,6 +45,21 @@ public class TranslatorEndpointTest {
         request.setLangTo("es");
         request.setText("This is a test of translation service");
         Object response = new WebServiceTemplate(marshaller).marshalSendAndReceive("http://localhost:" + port + "/ws", request);
+        assertNotNull(response);
+        assertThat(response, instanceOf(GetTranslationResponse.class));
+        GetTranslationResponse translation = (GetTranslationResponse) response;
+        assertThat(translation.getTranslation(), is("I don't know how to translate from en to es the text 'This is a test of translation service'"));
+    }
+
+    // Testing Remote Service failures
+    // In this case we have used a non existing URI to accomplish this goal (/remote-service-failure)
+    @Test(expected = WebServiceTransportException.class)
+    public void testRemoteServiceFailure() {
+        GetTranslationRequest request = new GetTranslationRequest();
+        request.setLangFrom("en");
+        request.setLangTo("es");
+        request.setText("This is a test of translation service");
+        Object response = new WebServiceTemplate(marshaller).marshalSendAndReceive("http://localhost:" + port + "/remote-service-failure", request);
         assertNotNull(response);
         assertThat(response, instanceOf(GetTranslationResponse.class));
         GetTranslationResponse translation = (GetTranslationResponse) response;
